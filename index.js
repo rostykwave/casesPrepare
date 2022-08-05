0a0d331a937417c9f0cb8db8d88ab5cb59bda964
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-08-05 17:10:42 +0300

    unique string set

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
-function flatten(array) {
-  const newArray = [];
-
-  for (let i = 0; i < array.length; i += 1) {
-    if (Array.isArray(array[i])) {
-      const flat = flatten(array[i]);
-
-      for (let j = 0; j < flat.length; j++) {
-        newArray.push(flat[j]);
-      }
-    } else {
-      newArray.push(array[i]);
-    }
-  }
-  return newArray;
+function removeDupes(str) {
+  return Array.from(new Set(str)).join('');
 }
 
-console.log(flatten([1, [1], [2, 3], [[[4, 5]]]]));
+console.log(removeDupes('abcd'));
+console.log(removeDupes('abcdabcd56865'));
