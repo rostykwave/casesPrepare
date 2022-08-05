cb687aff0e2dcfce593bf942fb7e4467943419b3
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-08-05 16:55:24 +0300

    aray of arrays flatten

--- index.js
+++ index.js
@@ -1,52 +1,18 @@
-// const rps = (p1, p2) => {
-//   const game = p1 + ' ' + p2;
-//   let winner = '';
-//   let message = '';
+function flatten(array) {
+  const newArray = [];
 
-//   if (
-//     game === 'rock rock' ||
-//     game === 'scissors scissors' ||
-//     game === 'paper paper'
-//   ) {
-//     winner = 'draw';
-//   } else if (
-//     game === 'rock scissors' ||
-//     game === 'scissors paper' ||
-//     game === 'paper rock'
-//   ) {
-//     winner = '1';
-//   } else {
-//     winner = '2';
-//   }
+  for (let i = 0; i < array.length; i += 1) {
+    if (Array.isArray(array[i])) {
+      const flat = flatten(array[i]);
 
-//   switch (winner) {
-//     case '1':
-//       message = `Player ${winner} won!`;
-//       break;
-//     case '2':
-//       message = `Player ${winner} won!`;
-//       break;
-
-//     default:
-//       message = 'Draw!';
-//       break;
-//   }
-
-//   return message;
-// };
-
-const rps = (p1, p2) => {
-  if (p1 === p2) return 'Draw!';
-  var rules = { rock: 'scissors', paper: 'rock', scissors: 'paper' };
-  if (p2 === rules[p1]) {
-    return 'Player 1 won!';
-  } else {
-    return 'Player 2 won!';
+      for (let j = 0; j < flat.length; j++) {
+        newArray.push(flat[j]);
+      }
+    } else {
+      newArray.push(array[i]);
+    }
   }
-};
+  return newArray;
+}
 
-console.log(rps('rock', 'scissors'));
-console.log(rps('scissors', 'rock'));
-console.log(rps('scissors', 'paper'));
-console.log(rps('paper', 'rock'));
-console.log(rps('paper', 'paper'));
+console.log(flatten([1, [1], [2, 3], [[[4, 5]]]]));
