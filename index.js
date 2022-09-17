2ad67872677c4bcd7c77437c216bf36ed5706c5d
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-09-17 22:12:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
+// var a = {
+//   a: 1,
+//   getContext: function () {
+//     console.log('this from a', this);
+//   },
+// };
+// var b = {
+//   a: 2,
+//   getContext: console.log('this from b', this),
+// };
+
+// a.getContext();
+// b.getContext;
+
 var a = {
-  a: 1,
+  b: 1,
   getContext: function () {
     console.log('this from a', this);
   },
 };
-var b = {
-  a: 2,
-  getContext: console.log('this from b', this),
+var d = {
+  prop: 'here',
 };
 
-a.getContext();
-b.getContext;
+var b = a.getContext.call(d); //
+var c = a.getContext.apply(d);
+var g = a.getContext.bind(d); //передає лише посилання на функціюз прив'язаним контекстом, а не викликає її як call and apply
+
+console.log('b', b);
+console.log('c', c);
+console.log('g', g);
